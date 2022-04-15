d41486e4fdb876eeff597800908c3554760b9894
Dong Nguyen <dongnguyen9186@gmail.com>
2022-04-15 15:23:51 +0700

    update typo token

--- src/theming/theme.ts
+++ src/theming/theme.ts
@@ -10,14 +10,9 @@ export interface ITheme {
     xl: string;
   };
   fontWeights: {
-    thin: number;
-    extralight: number;
-    light: number;
     regular: number;
     medium: number;
     semibold: number;
-    bold: number;
-    extrabold: number;
   };
   iconSizes: {
     sm: string;
@@ -32,52 +27,33 @@ export interface ITheme {
     xl: string;
     xxl: string;
   };
-  whiteSpaces: {
-    xs: string;
-    sm: string;
-    md: string;
-    lg: string;
-    xl: string;
-  };
   palette: Record<string, Hue>;
 }
 
 const fontSizes = {
-  xs: '10px',
-  sm: '12px',
-  md: '14px',
-  lg: '16px',
-  xl: '20px',
-  xxl: '28px',
+  xs: '11px',
+  sm: '13px',
+  md: '16px',
+  lg: '20px',
+  xl: '28px',
+  xxl: '32px',
 };
 
 const fontWeights = {
-  thin: 100,
-  extralight: 200,
-  light: 300,
   regular: 400,
   medium: 500,
   semibold: 600,
-  bold: 700,
-  extrabold: 800,
+
 };
 
 const lineHeights = {
-  xs: `15px`,
-  sm: `18px`,
-  md: `21px`,
-  lg: `24px`,
-  xl: `25px`,
-  xxl: `35px`
+  xs: `17px`,
+  sm: `19px`,
+  md: `24px`,
+  lg: `25px`,
+  xl: `35px`,
+  xxl: `40px`
 };
-
-const whiteSpaces = {
-  xs: '4px',
-  sm: '8px',
-  md: '12px',
-  lg: '16px',
-  xl: '20px',
-}
 
 const iconSizes = {
   xs: '12px',
@@ -95,7 +71,6 @@ const DEFAULT_THEME: ITheme = {
   iconSizes,
   lineHeights,
   palette: PALETTE,
-  whiteSpaces
 };
 
 /** @component */
