a48f7d4ada5972bf71691417f62a54ad3dcf5d03
Dong Nguyen <dongnguyen9186@gmail.com>
2022-04-18 09:56:26 +0700

    adding knobs

--- src/components/badge/Badge.stories.tsx
+++ src/components/badge/Badge.stories.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
-import { withKnobs, number, boolean, select, text } from '@storybook/addon-knobs';
-import Badge from './Badge';
+import React from "react";
+import {
+  withKnobs,
+  number,
+  boolean,
+  select,
+  text,
+} from "@storybook/addon-knobs";
+import Badge from "./Badge";
 
 export default {
-  title: '3. Components/Badge',
+  title: "3. Components/Badge",
   component: Badge,
   decorators: [withKnobs],
 };
 
-export const basic = () => (
-  <div>
-    <div
-      style={{ display: "flex", width: 200, justifyContent: "space-between" }}
-    >
-      <Badge count={100} type="primary" />
-      <Badge count={100} type="secondary" />
-      <Badge count={100} type="danger" />
-      <Badge count={100} />
-    </div>
-    <div style={{ display: "flex", marginTop: 20 }}>
-      <div style={{ margin: "0 10px" }}>
-        <Badge dot type="primary" />
-      </div>
-      <div style={{ margin: "0 10px" }}>
-        <Badge dot type="danger" />
-      </div>
-      <div style={{ margin: "0 10px" }}>
-        <Badge dot type="success" />
-      </div>
-    </div>
-  </div>
+export const count = () => (
+  <Badge
+    type={select(
+      "type",
+      ["primary", "secondary", "danger", "default", 'success'],
+      "primary"
+    )}
+    count={number("count", 100)}
+    showZero={boolean("showZero", false)}
+    overflowCount={number("overflowCount", 99)}
+  />
+);
+
+export const dot = () => (
+  <Badge
+    dot
+    type={select(
+      "type",
+      ["primary", "secondary", "danger", "default", "success"],
+      "primary"
+    )}
+  />
 );
