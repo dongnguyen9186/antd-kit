f39ed1f17e53a4fcbec5a6ef06a995ab1f2f223e
Dong Nguyen <dongnguyen9186@gmail.com>
2022-04-18 09:58:27 +0700

    remove unused things

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,6 +1,4 @@
 import React, { forwardRef } from "react";
-import PropTypes from "prop-types";
-import stylePropType from "react-style-proptype";
 import { Badge as AntBadge } from "antd";
 import StyledBadge, { getColor } from "./Badge.styled";
 import { IBadgeProps } from "./types";
